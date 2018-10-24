018db6cfbaa8bba5b3dae2c9ccc39b1cb19d6415
cherryApp <j.cserko@gmail.com>
2018-10-24 10:57:59 +0200

    Feat: add primitives.

---
+++ JS/variables/primitives.js
@@ -0,0 +1,19 @@
+// Number.
+var age = 22;
+console.log(typeof age);
+
+// Boolean.
+var check = true;
+
+// String.
+var name = 'Joe';
+console.log(name.indexOf('Jo'));
+
+{
+// Undefined.
+let something; // don't hoisted to the top of main scope
+console.log(typeof something);
+}
+
+var senki = null;
+console.log( senki != null );

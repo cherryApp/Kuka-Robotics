8b6c01a1f5086f4078bb2f8b2da233a65015ec25
cherryApp <j.cserko@gmail.com>
2018-10-26 09:14:35 +0200

    Feat: node server.

---
+++ NodeJS/basic/helloServer.js
@@ -0,0 +1,43 @@
+// Load modules.
+const http = require('http'),
+      path = require('path'),
+      fs = require('fs');
+
+const server = http.createServer( (request, response) => {
+    console.log(`Method: ${request.method}, Url: ${request.url}`);
+    router(request, response);
+});
+
+const router = (req, res) => {
+    let filePath = path.join(__dirname, 'template');
+    if (req.url == '/' || req.url == 'index') {
+        filePath = path.join(filePath, 'index.html');
+    } else {
+        filePath = path.join(filePath, req.url+'.html');
+    }
+
+    readHTML(filePath, (err, data) => {
+        if (err) {
+            return res.end('Error in request.');
+        }
+        res.end(data);
+    });
+};
+
+const readHTML = (filePath, callBack) => {
+    // let filePath = path.join(__dirname, 'users.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`File not found: ${filePath}`);
+            return callBack(err);
+        }
+        callBack(null, data);
+    });
+
+
+
+};
+
+server.listen(3333, () => {
+    console.log('Server is running...');
+});
